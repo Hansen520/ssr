2976a7e67e8498caaa03548245406c1b68e2e014
H <744769098@qq.com>
2024-01-19 10:50:43 +0800

    test

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+/*
+ * @Date: 2024-01-19 09:47:08
+ * @Description: description
+ */
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true, // ADD, 支持对commonjs全局变量的识别
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-var-requires": "off",
+  },
+  settings: {
+    react: { pragma: 'React', version: "detect" },
+  }
+};
---
+++ config/webpack.base.js
@@ -0,0 +1,32 @@
+/*
+ * @Date: 2024-01-19 10:09:21
+ * @Description: description
+ */
+// webpack.base.js
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-env"],
+        },
+      },
+      {
+        test: /.(ts|tsx)?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      "@": path.resolve(process.cwd(), "./src"),
+    },
+  },
+};
